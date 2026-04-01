66098a4ac7c5cac821ff0f20d15b9ea3ffb70316
Najwa Aulia Sassykirana <kirananajwa092@gmail.com>
2026-04-01 15:17:03 +0700

    feat: POS system, HR module, role-based access, Fidyatama theme

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,11 +7,6 @@ import { PrismaClient } from '@prisma/client'
 const prismaClientSingleton = () => {
   return new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
   })
 }
 
